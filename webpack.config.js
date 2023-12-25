691b089d8b7813a73cc1ae62a65ce296e382b78f
Denis Karnafel <denisk9999@mail.ru>
2023-12-25 22:22:14 +0700

    update webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   mode: process.env.NODE_ENV || 'development',
