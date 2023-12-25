4447f17d96e91e87399d1d94e9235459b08fed37
Denis Karnafel <denisk9999@mail.ru>
2023-12-26 01:43:07 +0700

    initialize i18next

---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    success: 'RSS Успешно загружен',
+    fail: {
+      notContainRss: 'Ресурс не содержит валидный RSS',
+      invalid: 'Ссылка должна быть валидным URL',
+      alreadyExists: 'RSS уже существует',
+      emptyForm: 'Заполните форму',
+    },
+  },
+};
