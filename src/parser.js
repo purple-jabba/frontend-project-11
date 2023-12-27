732f6ef58d9d261b46f4105079efb146f28a46ce
Denis Karnafel <denisk9999@mail.ru>
2023-12-27 18:38:26 +0700

    update parser.js

---
+++ src/parser.js
@@ -0,0 +1,32 @@
+import { uniqueId } from 'lodash';
+
+export default (xml) => {
+  const parsedRss = new DOMParser().parseFromString(xml, 'text/xml');
+  const checkError = parsedRss.querySelector('parsererror');
+  if (checkError) {
+    const error = new Error('rss is not found');
+    throw error;
+  }
+
+  const feedId = uniqueId();
+  const feed = {
+    id: feedId,
+    title: parsedRss.querySelector('title').textContent,
+    description: parsedRss.querySelector('description').textContent,
+  };
+
+  const posts = [];
+  const items = parsedRss.querySelectorAll('item');
+  items.forEach((item) => {
+    const post = {
+      id: uniqueId(),
+      feedId,
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent,
+    };
+
+    posts.push(post);
+  });
+  return { feed, posts };
+};
