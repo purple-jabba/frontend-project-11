034d5790c03e36abbc00395497970e9da55ef65b
Denis Karnafel <denisk9999@mail.ru>
2023-12-30 03:54:18 +0700

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,4 @@
-import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'node:path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default {
   mode: process.env.NODE_ENV || 'development',
@@ -35,10 +30,6 @@ export default {
     ],
   },
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
